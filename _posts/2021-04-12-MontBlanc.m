4b0bb64dff840ac83fe20f8d7ef83370d137d9b1
Antoine Mercier <75066484+AntoineMercier@users.noreply.github.com>
2021-04-18 08:47:19 +0200

    Update 2021-04-12-MontBlanc.m

--- _posts/2021-04-12-MontBlanc.m
+++ _posts/2021-04-12-MontBlanc.m
@@ -13,3 +13,6 @@ Références :
 Alpine thermal and structural evolution of the highest external crystalline massif: The Mont Blanc,
 Leloup, P. H., N. Arnaud, E. R. Sobel,and R. Lacassin (2005),
 Tectonics, 24, TC4002, doi:10.1029/2004TC001676.
+
+Une carte gravimétrique haute résolution du massif du Mont-Blanc - implications structurales,
+Masson F., F. Gal, P.H. Leloup, Comptes rendus Geoscience, vol. 333, issue 14, p. 1011-1019, 2002.
